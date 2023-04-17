afe2bb9f2d502533c263cdbd23fc88618f2c9a28
Stephen Bell <stephen@steedokc.com>
2023-04-17 13:50:26 -0500

    Try to keep entire cache instead of just puppeteer

--- plugins/keep-data-cache/index.js
+++ plugins/keep-data-cache/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   async onPreBuild({ utils }) {
-    await utils.cache.restore('/opt/buildhome/.cache/puppeteer/');
+    await utils.cache.restore('/opt/buildhome/.cache/');
   },
 
   async onPostBuild({ utils }) {
-    await utils.cache.save('/opt/buildhome/.cache/puppeteer/');
+    await utils.cache.save('/opt/buildhome/.cache/');
   }
 };
